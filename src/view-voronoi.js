f2f7a928bfd87abdc31919e3e8e84332034275c7
Forrest Oliphant <forrest@sembiki.com>
2016-11-01 12:18:00 -0400

    debug voronoi dots

--- src/view-voronoi.js
+++ src/view-voronoi.js
@@ -4,8 +4,15 @@ function renderPolygon (polygon, index, array) {
   const fill = `hsl(${index / array.length * 360}, 100%, 75%)`
   return html`
     <polygon
-      points="${polygon.map((point) => point.join(',')+' ')}"
+      points="${polygon.map((point) => point.join(',') + ' ')}"
       fill="${fill}" stroke="white" />
+  `
+}
+
+function renderDot (polygon) {
+  const {data} = polygon
+  return html`
+    <circle cx="${data[0]}" cy="${data[1]}" r="3" fill="white" />
   `
 }
 
@@ -14,6 +21,7 @@ function viewVoronoi (voronoi) {
   return html`
     <g>
       ${polygons.map(renderPolygon)}
+      ${polygons.map(renderDot)}
     <g>
   `
 }
